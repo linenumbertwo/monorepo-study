928a4c42665e95f2fbca76ebaa6e4c387b1eb741
linenumbertwo <imhoseonlee@gmail.com>
2024-07-05 11:26:40 +0900

    init

---
+++ packages/service/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Button } from "@if-monorepo/design-system";
+
+function App() {
+  return (
+    <div>
+      <Button>TEST</Button>
+    </div>
+  );
+}
+
+export default App;
